c80edc32e35070031764e6543e8cbfe82da56bf8
Unknown <luismiguel92_@hotmail.com>
2018-02-15 21:47:13 -0500

    programa inicial

---
+++ Main.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+
+        public static void main (String[] args){
+
+                Scanner digito = new Scanner (System.in);
+                
+                int numero;
+                int dos= 2;
+
+                    
+                  System.out.println("Ejercicio # 3");
+                  System.out.println("");
+
+                     System.out.println("Este ejercicio le va a permitir determinar si un numero ingresado es primo o no");
+                     System.out.println("Digite un numero");
+                     numero = digito.nextInt();
+
+
+                         
+
+        }
+
+
+
+}
